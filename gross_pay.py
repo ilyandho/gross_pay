34397219eaa789d89ad0f0ad9353238efd370abb
ilyandho <ii.ndhote@gmail.com>
2019-05-25 20:16:09 +0300

    Added try-except error handler

--- gross_pay.py
+++ gross_pay.py
@@ -1,11 +1,29 @@
-hrs = float(input('Enter Hours:'))
-rate = float(input('Enter Rate:'))
+cont = True
 
-if hrs <= 40:
+# Convert hours to float
+while cont:
+    hrs = input('Enter Hours:')
+    try:
+        hours = float(hrs)
+        cont = False
+    except:
+        print('Error: Please eater a numeric input')
+
+# Convert Rate to float
+cont = True
+while cont:
+    rate = input('Enter Rate:')
+    try:
+        rate = float(rate)
+        cont = False
+    except:
+        print('Error: Please eater a numeric input')
+
+print(hours)
+if hours <= 40:
     # Normal rating for hours <= 40
-    pay = hrs*rate
+    pay = hours*rate
 else:
     # Give the employee 1.5 times the hourly rate for hours worked above 40 hours
-    pay = 40*rate + (hrs-40)*1.5*10
-
+    pay = 40*rate + (hours-40)*1.5*10
 print('Pay:', pay)
