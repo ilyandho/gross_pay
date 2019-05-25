1010f63a362df1383cbcf654592aabb5195ef68c
ilyandho <ii.ndhote@gmail.com>
2019-05-25 20:03:56 +0300

    Added payment for the extra hours work beyond 40 hours

--- gross_pay.py
+++ gross_pay.py
@@ -1,6 +1,11 @@
 hrs = float(input('Enter Hours:'))
 rate = float(input('Enter Rate:'))
 
-pay = hrs*rate
+if hrs <= 40:
+    # Normal rating for hours <= 40
+    pay = hrs*rate
+else:
+    # Give the employee 1.5 times the hourly rate for hours worked above 40 hours
+    pay = 40*rate + (hrs-40)*1.5*10
 
 print('Pay:', pay)
